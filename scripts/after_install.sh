7a6a96547a3e2da3143257bdec4e80e17778dfa3
Jenish Kothari <kotharijenish2001@gmail.com>
2024-07-11 08:57:18 -0400

    Update in scripts.

---
+++ scripts/after_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Any post-installation tasks can go here
+echo "After install tasks"
---
+++ scripts/before_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Ensure the deployment directory exists
+mkdir -p /home/ec2-user/frontend-deployment
